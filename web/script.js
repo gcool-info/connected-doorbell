6234ad59320ee920c1d7fa2dbcff3efa6c13e07b
gcool-info <george.koulouris1@gmail.com>
2014-12-03 15:43:48 +0000

    Alarm set-off when visiting page

--- web/script.js
+++ web/script.js
@@ -1,25 +1,20 @@
 $( document ).ready(function() {
+	/* Start by setting off the alarm */
 	$.ajax({
 		url: 'ajax.php',
 		type: 'POST',
-		dataType: "json",
 		data: {
-			"cmd": 'read'
+			"cmd": 'turn_on'
 		},
-		success: function(data) {
-			if (data == 'off') {
-				$('.title').hide(1000);
-				$('.icon').css('opacity',1);
-				$('.icon').removeClass('disabled');
-			} else {
-				$('.title').show(1000);
-				$('.icon').css('opacity', 0.2);
-				$('.icon').addClass('disabled');
-			}	
-		},
+		success: function() {
+			$('.title').show(1000);
+			$('.icon').css('opacity', 0.2);
+			$('.icon').addClass('disabled');
+		}
 	});
 	
 
+	/* Set-off the alarm on click */
 	$('.icon').click(function() {
 
 		if ($('.icon').hasClass('disabled'))
@@ -44,7 +39,7 @@ $( document ).ready(function() {
 	});
 
 
-
+	/* Check the alarm state every 10s */
 	setInterval(function(){
 
 		
