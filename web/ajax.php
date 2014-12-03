27a836e55395d92fbe8868c5adb689def5dc9432
gcool-info <george.koulouris1@gmail.com>
2014-12-03 13:51:22 +0000

    Ajax call to see whether the sound has played

--- web/ajax.php
+++ web/ajax.php
@@ -1,10 +1,48 @@
 <?php
-	/* open the txt file */
-	$handle = fopen("status.txt", "w");
 
-	/* Write the status of the heater in the txt file */
-	fwrite($handle, $_POST["status"]);
+	$command = $_GET["cmd"];
 
-	/* close the txt file */
-	fclose($handle);
+	switch ($command) {
+		case 'turn_on':
+
+			/* open the txt file */
+			$handle = fopen("status.txt", "w");
+
+			/* Write the status of the heater in the txt file */
+			fwrite($handle, 'on');
+
+			/* close the txt file */
+			fclose($handle);
+
+			break;
+
+		case 'turn_off': 
+
+			/* open the txt file */
+			$handle = fopen("status.txt", "w");
+
+			/* Write the status of the heater in the txt file */
+			fwrite($handle, 'off');
+
+			/* close the txt file */
+			fclose($handle);
+
+			break;
+
+		case 'read':
+
+			/* open the txt file */
+			$handle = fopen("status.txt", "r");
+
+			/* Check the status */
+			$results = fread($handle,filesize("status.txt"));
+
+			/* close the txt file */
+			fclose($handle);
+
+			die(json_encode($results));
+
+			break;
+	}
+	
 ?>
--- web/script.js
+++ web/script.js
@@ -1,36 +1,46 @@
-$('.icon').click(function() {
+$( document ).ready(function() {
+	$('.icon').click(function() {
 
-	if ($('.icon').hasClass('disabled'))
-		return;
+		if ($('.icon').hasClass('disabled'))
+			return;
 
-	$('.title').show(1000);
-	$('.icon').css('opacity', 0.2);
-	$('.icon').addClass('disabled');
-
-	$.ajax({
-		url: 'ajax.php',
-		type: 'POST',
-		dataType: "json",
-		timeout: 10000,
-		data: {
-			"status": 'on'
-		}
-	});
-
-	setTimeout(function(){
-		$('.title').hide(1000);
-		$('.icon').css('opacity',1);
-		$('.icon').removeClass('disabled');
+		$('.title').show(1000);
+		$('.icon').css('opacity', 0.2);
+		$('.icon').addClass('disabled');
 
 		$.ajax({
 			url: 'ajax.php',
-			type: 'POST',
+			type: 'GET',
 			dataType: "json",
 			timeout: 10000,
 			data: {
-				"status": 'off'
+				"cmd": 'turn_on'
 			}
 		});
+	});
 
-	}, 15000);
+	setInterval(function(){
+
+			$.ajax({
+				url: 'ajax.php',
+				type: 'GET',
+				dataType: "json",
+				timeout: 10000,
+				data: {
+					"cmd": 'read'
+				},
+				success: function(data) {
+					if (data == 'off') {
+						$('.title').hide(1000);
+						$('.icon').css('opacity',1);
+						$('.icon').removeClass('disabled');
+					} else {
+						$('.title').show(1000);
+						$('.icon').css('opacity', 0.2);
+						$('.icon').addClass('disabled');
+					}	
+				},
+			});
+
+		}, 1000);
 })
